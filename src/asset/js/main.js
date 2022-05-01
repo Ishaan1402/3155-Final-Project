1bf38e4cff095309655c24ba89369d86747d87b4
Ishaan1402 <73907472+Ishaan1402@users.noreply.github.com>
2022-05-01 13:12:17 -0400

    Update main.js

---
+++ src/asset/js/main.js
@@ -0,0 +1,4 @@
+$( "#submit" ).click(function() {
+    alert("It's a match! Click on this link to be redirected to a page filled with internships " +
+          "that match your specifications.\n\nwww.group17internships.com");
+});
